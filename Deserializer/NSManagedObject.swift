6955b564a430f0c7e36b7fbd592077902f1b43e7
Daniel Tull <dt@danieltull.co.uk>
2016-05-12 17:39:57 +0100

    Ensure the value is valid with respect to type and the standard validation methods

--- Deserializer/NSManagedObject.swift
+++ Deserializer/NSManagedObject.swift
@@ -16,8 +16,60 @@ extension NSManagedObject {
 				}
 
 			case .One(let object):
-				setValue(object, forKey: attribute.name)
+				validateAndSetValue(object, forAttribute: attribute)
 		}
+	}
+
+	private func validateAndSetValue(value: AnyObject, forAttribute attribute: NSAttributeDescription) {
+
+		// TYPE VALIDATION
+
+		guard
+			let object = value as? NSObject
+			where validateType(object: object, forAttribute: attribute)
+		else {
+			let entityName = entity.name ?? "Unknown Entity" // Unlikely to happen
+			let desiredType = attribute.attributeValueClassName ?? "Unknown Type"
+			print("[Deserializer] Unacceptable type of value for attribute: entity = \(entityName); property = \(attribute.name); desired type = \(desiredType); given type = \(value.dynamicType); value = \(value)")
+			return
+		}
+
+		// STANDARD VALIDATION
+
+		guard
+			validate(value: value, forAttribute: attribute)
+		else {
+			let entityName = entity.name ?? "Unknown Entity"
+			print("[Deserializer] Failed validation of value for attribute: entity = \(entityName); property = \(attribute.name); type = \(value.dynamicType); value = \(value)")
+			return
+		}
+
+		setValue(value, forKey: attribute.name)
+	}
+
+	private func validate(value value: AnyObject, forAttribute attribute: NSAttributeDescription) -> Bool {
+
+		do {
+			var v: AnyObject? = value
+			try validateValue(&v, forKey: attribute.name)
+			return true
+		} catch {
+			return false
+		}
+	}
+
+	private func validateType(object object: NSObject, forAttribute attribute: NSAttributeDescription) -> Bool {
+
+		// If there's no attributeValueClassName, the type is likely transformable.
+		// Either way we can't provide any further validation.
+		guard
+			let attributeClassName = attribute.attributeValueClassName,
+			let attributeClass = NSClassFromString(attributeClassName)
+		else {
+			return true
+		}
+
+		return object.isKindOfClass(attributeClass)
 	}
 
 	func set(value value: Value, relationship: NSRelationshipDescription, serializationInfo: SerializationInfo) {
